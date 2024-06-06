6a22c710d40416256cfaa19231bdb90a7e9c9141
Chinenye Anikwenze <anikwenzekelly@gmail.com>
2024-06-06 21:43:12 +0100

    set up env for usage

--- quickstart/src/main/java/com/kodervine_crud/quickstart/QuickstartApplication.java
+++ quickstart/src/main/java/com/kodervine_crud/quickstart/QuickstartApplication.java
@@ -1,5 +1,6 @@
 package com.kodervine_crud.quickstart;
 
+import com.kodervine_crud.quickstart.config.PizzaConfig;
 import com.kodervine_crud.quickstart.services.ColorPrinter;
 import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
@@ -11,17 +12,21 @@ import java.awt.*;
 @SpringBootApplication
 @Log
 public class QuickstartApplication implements CommandLineRunner {
-	private ColorPrinter colorPrinter;
+	final PizzaConfig pizzaConfig;
 
-	public QuickstartApplication(ColorPrinter colorPrinter){
-		this.colorPrinter = colorPrinter;
+	public QuickstartApplication(PizzaConfig pizzaConfig) {
+		this.pizzaConfig = pizzaConfig;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickstartApplication.class, args);
 	}
- @Override
+
+	@Override
 	public void run(final String... args) {
-		log.info(colorPrinter.print());
+		log.info(String.format("I want a %s crust pizza with %s and %s sauce",
+				pizzaConfig.getCrust(),
+				pizzaConfig.getTopping(),
+				pizzaConfig.getSauce()));
  }
 }
